33c260d239184ba156136552d80619a65228c08e
h3y6e <h3y6e@pm.me>
2022-06-05 16:20:33 +0900

    add block_id

---
+++ src/filemanager/block_id.rs
@@ -0,0 +1,22 @@
+#[derive(Debug, PartialEq, Eq, Hash)]
+pub struct BlockId {
+  filename: String,
+  blknum: u64,
+}
+
+impl BlockId {
+  pub fn new(filename: String, blknum: u64) -> Self {
+    BlockId {
+      filename,
+      blknum,
+    }
+  }
+
+  pub fn filename(&self) -> String {
+    self.filename.clone()
+  }
+
+  pub fn number(&self) -> u64 {
+    self.blknum
+  }
+}
