f57d1b3024304c1baf7d35f31e592887c198eeb2
panorama32 <mitsueda.itsuki.bc@gmail.com>
2022-09-18 19:11:44 +0900

    adjust test

--- tests/logmgr_test.rs
+++ tests/logmgr_test.rs
@@ -1,29 +1,31 @@
-// use simpledb::filemanager::page::{New, Page};
-// use simpledb::{filemanager::file_mgr::FileMgr, memorymanager::log_manager::LogMgr};
+use simpledb::filemanager::page::{New, Page};
+use simpledb::{filemanager::file_mgr::FileMgr, memorymanager::log_manager::LogMgr};
 
-// extern crate simpledb;
+use std::cell::RefCell;
 
-// #[test]
-// pub fn log_mananger_test() {
-//     let fm = FileMgr::new("testdata", 400).unwrap();
-//     let lm = LogMgr::new(fm, "log_manager_test".to_string());
+extern crate simpledb;
 
-//     append_records(&lm, 1, 70);
-//     lm.flush(70).unwrap();
-// }
+#[test]
+pub fn log_mananger_test() {
+    let fm = FileMgr::new("testdata", 400).unwrap();
+    let mut lm = LogMgr::new(RefCell::from(fm), "log_manager_test").unwrap();
 
-// fn append_records(lm: &LogMgr, start: i32, end: i32) {
-//     for i in start..=end {
-//         let rec = create_record(format!("record{}", i), i);
-//         lm.append(rec).unwrap();
-//     }
-// }
+    append_records(&mut lm, 1, 70);
+    lm.flush(70).unwrap();
+}
 
-// fn create_record(s: String, n: i32) -> Vec<u8> {
-//     let npos = Page::max_length(s.len());
-//     let b = Vec::with_capacity(npos);
-//     let mut p = Page::new(b);
-//     p.set_string(0, s).unwrap();
-//     p.set_i32(npos, n).unwrap();
-//     return p.contents().clone();
-// }
+fn append_records(lm: &mut LogMgr, start: i32, end: i32) {
+    for i in start..=end {
+        let rec = create_record(format!("record{}", i), i);
+        lm.append(rec).unwrap();
+    }
+}
+
+fn create_record(s: String, n: i32) -> Vec<u8> {
+    let npos = Page::max_length(s.len());
+    let b = Vec::with_capacity(npos);
+    let mut p = Page::new(b);
+    p.set_string(0, s).unwrap();
+    p.set_i32(npos, n).unwrap();
+    return p.contents().clone();
+}
