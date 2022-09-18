70f1d5fc1a159d39642eece463698eddbc457312
soichiro-ueda <soichiro-ueda@cookpad.com>
2022-09-18 16:23:12 +0900

    add logmgr_test

--- tests/logmgr_test.rs
+++ tests/logmgr_test.rs
@@ -5,24 +5,25 @@ extern crate simpledb;
 
 #[test]
 pub fn log_mananger_test() {
-    let fm = FileMgr::new("testdata", 30).unwrap();
+    let fm = FileMgr::new("testdata", 400).unwrap();
     let lm = LogMgr::new(fm, "log_manager_test");
 
-    lm.
+    append_records(&lm, 1, 70);
+    lm.flush(70).unwrap();
 }
 
 fn append_records(lm: &LogMgr, start: i32, end: i32) {
     for i in start..=end {
         let rec = create_record(format!("record{}", i), i);
-        let lsn = lm.append(rec);
+        lm.append(rec).unwrap();
     }
 }
 
 fn create_record(s: String, n: i32) -> Vec<u8> {
     let npos = Page::max_length(s.len());
     let b = Vec::with_capacity(npos);
-    let p = Page::new(b);
+    let mut p = Page::new(b);
     p.set_string(0, s).unwrap();
     p.set_i32(npos, n).unwrap();
-    return b;
+    return p.contents().clone();
 }
